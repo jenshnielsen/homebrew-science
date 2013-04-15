78af29fca248cf662577069ae3967953ebfdcfa5
Andrew Janke <janke@pobox.com>
2013-04-15 15:20:52 -0400

    suite-sparse: fix CHOLMOD in 4.1.0 with -DNCAMD, change dep to metis4
    In SuiteSparse 4.1.0, the -DNPARTITION option was partially split out in
    to -DNCAMD. However, looks like there's a bug in the Makefile, so when METIS
    is not linked, libcholmod.a does not get properly built due to an error in
    CAMD-related build setup. Add -DNCAMD to match the 4.0.2 behavior and get
    it building again.
    
    Also change the metis dependency to use the metis4 formula instead of metis,
    since SuiteSparse is not compatible with Metix 5.x.

--- suite-sparse.rb
+++ suite-sparse.rb
@@ -6,25 +6,26 @@ class SuiteSparse < Formula
   sha1 '93a0ae741b399d0dbecd43235d2f8977cdd9bc47'
 
   option "without-tbb", "Do not link with tbb (Threading Building Block)"
-  option "with-metis", "Compile in metis libraries"
+  option "with-metis", "Compile in metis 4.x libraries"
   option "with-openblas", "Use openblas instead of Apple's Accelerate.framework"
 
   depends_on "tbb" unless build.include? "without-tbb"
   # Metis is optional for now because of
   # cholmod_metis.c:164:21: error: use of undeclared identifier 'idxtype'
-  depends_on "metis" if build.include? "with-metis"
+  depends_on "metis4" if build.include? "with-metis"
   depends_on "openblas" if build.include? "with-openblas"
 
   def install
     # SuiteSparse doesn't like to build in parallel
     ENV.j1
 
-    inreplace 'SuiteSparse_config/SuiteSparse_config.mk' do |s|
+    # Switch to the Mac base config, per SuiteSparse README.txt
+    system "mv SuiteSparse_config/SuiteSparse_config.mk SuiteSparse_config/SuiteSparse_config_orig.mk"
+    system "mv SuiteSparse_config/SuiteSparse_config_Mac.mk SuiteSparse_config/SuiteSparse_config.mk"
+    
+    inreplace "SuiteSparse_config/SuiteSparse_config.mk" do |s|
       if build.include? 'with-openblas'
         s.change_make_var! "BLAS", "-lopenblas"
-        s.change_make_var! "LAPACK", "$(BLAS)"
-      else
-        s.change_make_var! "BLAS", "-Wl,-framework -Wl,Accelerate"
         s.change_make_var! "LAPACK", "$(BLAS)"
       end
 
@@ -33,9 +34,12 @@ class SuiteSparse < Formula
         s.change_make_var! "TBB", "-ltbb"
       end
 
-      if build.include? 'with-metis'
+      if build.include? "with-metis"
         s.remove_make_var! "METIS_PATH"
-        s.change_make_var! "METIS", Formula.factory("metis").lib + "libmetis.a"
+        s.change_make_var! "METIS", Formula.factory("metis4").lib + "libmetis.a"
+      else
+        # Use -DNCAMD to work around apparent bug in -I setup, matching 4.0.2 behavior
+        s.change_make_var! "CHOLMOD_CONFIG", "-DNCAMD"
       end
 
       s.change_make_var! "INSTALL_LIB", lib
