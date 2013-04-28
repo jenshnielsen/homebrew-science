2f8b02ad852694e59000ee8dea7998ed3fc3beb6
Jens H. Nielsen <jens.nielsen@ucl.ac.uk>
2013-04-28 21:27:18 +0100

    add first version of trng formula.

---
+++ trng.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Trng < Formula
+  homepage 'http://numbercrunch.de/trng/'
+  url 'http://numbercrunch.de/trng/trng-4.14.tar.gz'
+  sha1 '199876323e6d4bfdcd6b8cc9df2dd3c0d7ad3170'
+
+  def install
+    system "./configure"
+    system "make"
+    system "make install"
+  end
+end
